37b22a857a000fa9683d0bc6083e1c20613d0579
PalakMishraGit <palakmishra344@gmail.com>
2026-03-25 01:01:40 +0530

    chore: remove unnecessary files and update gitignore

--- readpdf2.cjs
+++
@@ -1,7 +0,0 @@
-const { PdfReader } = require('pdfreader');
-
-new PdfReader().parseFileItems('public/PalakMishraCV2026.pdf', (err, item) => {
-  if (err) console.error("error:", err);
-  else if (!item) console.log("EOF");
-  else if (item.text) console.log(item.text);
-});
